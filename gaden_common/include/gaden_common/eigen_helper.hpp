ac6f65495577d13ea00c81a0aadbcb64b83500d7
Dino Hüllmann <7059909+hdino@users.noreply.github.com>
2020-04-21 10:03:44 +0200

    Eigen toString support for Matrix and Array

--- gaden_common/include/gaden_common/eigen_helper.hpp
+++ gaden_common/include/gaden_common/eigen_helper.hpp
@@ -12,13 +12,32 @@ class Node;
 
 namespace gaden {
 
-template <typename T>
-inline std::string toString(const Eigen::Matrix<T, 3, 1> &vec, size_t indention = 0)
+namespace internal {
+
+template <typename Derived>
+std::string toStringFromEigenVec3Like(const Eigen::DenseBase<Derived> &vec, size_t indention)
 {
+    static_assert((Derived::RowsAtCompileTime == 1 && Derived::ColsAtCompileTime == 3) ||
+                  (Derived::RowsAtCompileTime == 3 && Derived::ColsAtCompileTime == 1));
+
     (void)indention;
     return "[" + std::to_string(vec[0]) + ", "
                + std::to_string(vec[1]) + ", "
                + std::to_string(vec[2]) + "]";
+}
+
+} // namespace internal
+
+template <typename T>
+inline std::string toString(const Eigen::Matrix<T, 3, 1> &vec, size_t indention = 0)
+{
+    return internal::toStringFromEigenVec3Like(vec, indention);
+}
+
+template <typename T>
+inline std::string toString(const Eigen::Array<T, 3, 1> &vec, size_t indention = 0)
+{
+    return internal::toStringFromEigenVec3Like(vec, indention);
 }
 
 } // namespace gaden
